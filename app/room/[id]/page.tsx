ef9ffe2423227b1fcc8851da94f162b61bcf3f37
surya-lohan <suryanshlohan@gmail.com>
2026-09-11 22:18:54 +0530

    Added route protection in room/[id]

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,13 +1,22 @@
 "use client";
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Whiteboard from "@/app/components/Whiteboard";
 import RoomContext from "@/app/Context/RoomContext";
 import CodeEditor from "@/app/components/CodeEditor";
 import Navbar from "@/app/components/Navbar";
 import { Group, Panel, Separator } from "react-resizable-panels";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export default function RoomPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = use(params);
+    const { data: session, isPending } = authClient.useSession();
+    const router = useRouter();
+    useEffect(() => {
+        if (!session && !isPending) {
+            router.push('/auth/signin')
+        }
+    }, [session, isPending, router]);
 
     return (
         <RoomContext roomId={id}>
@@ -19,7 +28,7 @@ export default function RoomPage({ params }: { params: Promise<{ id: string }> }
                             <CodeEditor />
                         </Panel>
                         <Separator className="w-2 bg-slate-900/80 hover:bg-slate-800 transition-colors cursor-col-resize flex items-center justify-center relative z-10 group">
-                            <div className="w-0.5 h-8 rounded-full bg-slate-700 group-hover:bg-[#8083FF] group-active:bg-[#8083FF] transition-colors" />
+                            <div className="w-0.5 h-8 rounded-full bg-slate-700 group-hover:bg-[#8083FF] transition-colors" />
                         </Separator>
                         <Panel defaultSize="50%" className="h-full w-full relative overflow-hidden">
                             <Whiteboard />
